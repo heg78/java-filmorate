349112a1c0288e39e034ed5c4c46ab76c5d7a032
heg78 <heg78@ymail.ru>
2024-02-20 10:51:07 +0300

    feat:добавить БД

--- src/main/java/ru/yandex/practicum/filmorate/dao/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/MpaDbStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.dao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
