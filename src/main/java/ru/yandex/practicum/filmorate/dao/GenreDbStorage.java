c2335b9693a067c354b5f5be21b9230f119e64c1
heg78 <heg78@ymail.ru>
2024-02-20 10:49:06 +0300

    feat:добавить БД

--- src/main/java/ru/yandex/practicum/filmorate/dao/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/GenreDbStorage.java
@@ -4,14 +4,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 
 @Component
-public class GenreDbStorage implements GenreStorage{
+public class GenreDbStorage implements GenreStorage {
     private final JdbcTemplate jdbcTemplate;
 
     public GenreDbStorage(JdbcTemplate jdbcTemplate) {
